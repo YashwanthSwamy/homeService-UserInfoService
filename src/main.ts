bda046b0a5913ad6ca6deb3291528fd97ba5d8b6
YashwanthSwamy <yasw2875@colorado.edu>
2022-11-26 21:47:37 -0700

    feat: Database intialisation

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,39 @@
 import bodyParser from "body-parser";
 import express from "express";
 import { environmentVariables } from "./configuration/environmentVariables";
+import { TableInitializer } from "./database/tables/intialization";
 import { userInfoRoutes } from "./routes/userInfoRoutes";
 
 const port = environmentVariables.PORT;
 const app = express();
 
 function checkErrors(err: any, req: any, res: any, next: any) {
-    // Catch JSON error
-    if (err instanceof SyntaxError && err.message.indexOf("JSON")) {
-      res.status(400).json({ errorCode: 100, Ref: "Error parsing JSON" });
-    } else {
-      res.status(400).json({ errorCode: 101, Ref: "Error parsing JSON" });
-    }
+  if (err instanceof SyntaxError && err.message.indexOf("JSON")) {
+    res.status(400).json({ errorCode: 100, Ref: "Error parsing JSON" });
+  } else {
+    res.status(400).json({ errorCode: 101, Ref: "Error parsing JSON" });
   }
+}
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(checkErrors);
 app.use(userInfoRoutes);
+
+async function main() {
+  await TableInitializer.init()
+    .then(async () => {
+      console.log("Database up !!!");
+      // return messageQ.init();
+    })
+    .then(async () => {
+      console.log("Broker up !!!");
+      console.log("Application started on port " + port);
+      app.listen(port);
+    })
+    .catch(async (error) => {
+      console.error("Error setting up service", { error: error });
+    });
+}
+
+main();
+export { app };
